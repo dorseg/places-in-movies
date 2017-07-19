36885a9b1bbf54f0a5d961dfb4193e40e4bff0fb
dorseg <dor.segal@gmail.com>
2017-07-20 00:35:54 +0300

    added crawler

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for elispider project
+# Scrapy settings for spider project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,14 +9,14 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'elispider'
+BOT_NAME = 'spider'
 
-SPIDER_MODULES = ['elispider.spiders']
-NEWSPIDER_MODULE = 'elispider.spiders'
+SPIDER_MODULES = ['crawler.spiders']
+NEWSPIDER_MODULE = 'crawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'elispider (+http://www.yourdomain.com)'
+#USER_AGENT = 'spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -47,13 +47,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'elispider.middlewares.ElispiderSpiderMiddleware': 543,
+#    'spider.middlewares.SpiderSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'elispider.middlewares.MyCustomDownloaderMiddleware': 543,
+#    'spider.middlewares.MyCustomDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'elispider.pipelines.ElispiderPipeline': 300,
+#    'spider.pipelines.SpiderPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
--- crawler/crawler/spiders/IMDBspider.py
+++ crawler/crawler/spiders/IMDBspider.py
@@ -1,36 +1,39 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
 
-from elispider.items import MovieItem
+from crawler.items import MovieItem
 
 IMDB_URL = "http://imdb.com"
 TOTAL_PAGES = 5  # modify to control the number of pages to follow
 PASSED_PAGES = 0 # count the number of pages already followed
 
-class IMDBSpider(scrapy.Spider):
+class IMDBSpider(CrawlSpider):
     name = 'imdb'
+    rules = (Rule(LinkExtractor(allow=(), restrict_xpaths=("//div[@class='nav']/div/a[@class='lister-page-next next-page']",)),
+                  callback="parse_page", follow= True),)
 
     def __init__(self, start=None, end=None, *args, **kwargs):
         super(IMDBSpider, self).__init__(*args, **kwargs)
         self.start_year = int(start) if start else 1874
-        self.end_year = int(end) if end else 2016
+        self.end_year = int(end) if end else 2017
 
     # generate start_urls dynamically
     def start_requests(self):
         print "=====================start_requests================================"
         for year in range(self.start_year, self.end_year+1):
             # sorted by number of votes
-            yield scrapy.Request('http://www.imdb.com/search/title?year={year},{year}&title_type=feature&sort=num_votes,desc'.format(year=year), callback=self.parse_page)
+            yield scrapy.Request('http://www.imdb.com/search/title?year={year},{year}&title_type=feature&sort=num_votes,desc'.format(year=year))
 
     def parse_page(self, response):
+        global TOTAL_PAGES, PASSED_PAGES
+        if PASSED_PAGES == TOTAL_PAGES:
+            raise CloseSpider("Reached page limit {}".format(TOTAL_PAGES))
         print "=====================parse_page================================"
-        next_page = response.xpath("//div[@class='nav']/div/a[@class='lister-page-next next-page']/@href").extract_first()
-        next_url = IMDB_URL + '/search/title' + next_page if next else None # next page might not appear
 
         content = response.xpath("//div[@class='lister-item-content']")
         paths = content.xpath("h3[@class='lister-item-header']/a/@href").extract() # list of paths of movies in the current page
-        # ids = [path[path.find('tt'):path.find('/?')] for path in paths] <<<<<<<<<<<<< REMOVE
 
         # for path in paths:
         #     item = MovieItem()
@@ -39,22 +42,26 @@ class IMDBSpider(scrapy.Spider):
         #     request.meta['item'] = item
         #     yield request
 
+        # single movie
         path = paths[0]
         item = MovieItem()
         item['MainPageUrl'] = IMDB_URL + path
-        item['NextPageUrl'] = next_url
         request = scrapy.Request(item['MainPageUrl'], callback=self.parse_movie_details)
         request.meta['item'] = item
+        PASSED_PAGES += 1
         yield request
+
+    parse_start_url = parse_page
 
     def parse_movie_details(self, response):
         print "=====================parse_movie_details================================"
         item = response.meta['item']
         synopsis_url = response.xpath("//a[text()='Plot Synopsis']/@href").extract()
         url = item['MainPageUrl']
+        title = response.xpath("//div[@class='titleBar']/div[@class='title_wrapper']/h1/text()").extract()[0][:-1]
         if synopsis_url:
-            item['ID'] = url[url.find('/tt')+1:url.find('/?')] # to ignore 'tt', change to +3
-            item['Title'] = response.xpath("//div[@class='titleBar']/div[@class='title_wrapper']/h1/text()").extract()[0][:-1]
+            item['ID'] = url[url.find('/tt')+3:url.find('/?')] # to ignore 'tt', change to +3
+            item['Title'] = title
             item['Year'] = response.xpath("//div[@class='titleBar']/div[@class='title_wrapper']/h1/span/a/text()").extract()[0]
             item['Director'] = response.xpath("//div/span[@itemprop='director']/a/span/text()").extract()
             item['Genres'] = response.xpath("//div[@itemprop='genre']/a/text()").extract()
@@ -66,17 +73,8 @@ class IMDBSpider(scrapy.Spider):
             request.meta['item'] = item
             yield request
 
-        # move to next page
         else:
-            print "=====================move_to_next_page================================"
-            global TOTAL_PAGES, PASSED_PAGES
-            if PASSED_PAGES == TOTAL_PAGES:  # already followed enough pages
-                PASSED_PAGES = 0  # reset
-                return
-            elif item['NextPageUrl']:
-                PASSED_PAGES += 1
-                request = scrapy.Request(item['NextPageUrl'], callback=self.parse_page)
-                yield request
+            print ">>>>>>>>>>>>>>>>>>>> Synopsis of \"{}\" NOT FOUND!!!!".format(title)
 
     def parse_synopsis(self,response):
         print "=====================parse_synopsis================================"
@@ -85,25 +83,5 @@ class IMDBSpider(scrapy.Spider):
         item['Synopsis'] = synopsis
         print item
 
-        # move to next page
-        print "=====================move_to_next_page================================"
-        global TOTAL_PAGES, PASSED_PAGES
-        if PASSED_PAGES == TOTAL_PAGES:  # already followed enough pages
-            PASSED_PAGES = 0  # reset
-            return
-        elif item['NextPageUrl']:
-            PASSED_PAGES += 1
-            request = scrapy.Request(item['NextPageUrl'], callback=self.parse_page)
-            yield request
 
-    def move_to_next_page(self, next_url):
-        print "=====================move_to_next_page================================"
-        global TOTAL_PAGES, PASSED_PAGES
-        if PASSED_PAGES == TOTAL_PAGES: # already followed enough pages
-            PASSED_PAGES = 0 # reset
-            return
-        elif next_url:
-            PASSED_PAGES += 1
-            request = scrapy.Request(next_url, callback=self.parse_page)
-            yield request
 
