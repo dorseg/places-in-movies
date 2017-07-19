2384e3f5b5f39cbb4b1df7de1751f4914811ec57
elisim <elisimhayev@gmail.com>
2017-07-20 00:56:33 +0300

    added -a total in command line

--- crawler/crawler/spiders/IMDBspider.py
+++ crawler/crawler/spiders/IMDBspider.py
@@ -6,16 +6,16 @@ from scrapy.exceptions import CloseSpider
 from crawler.items import MovieItem
 
 IMDB_URL = "http://imdb.com"
-TOTAL_PAGES = 5  # modify to control the number of pages to follow
-PASSED_PAGES = 0 # count the number of pages already followed
 
 class IMDBSpider(CrawlSpider):
     name = 'imdb'
     rules = (Rule(LinkExtractor(allow=(), restrict_xpaths=("//div[@class='nav']/div/a[@class='lister-page-next next-page']",)),
                   callback="parse_page", follow= True),)
 
-    def __init__(self, start=None, end=None, *args, **kwargs):
+    def __init__(self, start=None, end=None, total=None, *args, **kwargs):
         super(IMDBSpider, self).__init__(*args, **kwargs)
+        self.passed_pages = 0 # count the number of pages already followed
+        self.total_pages = int(total) if total else 5
         self.start_year = int(start) if start else 1874
         self.end_year = int(end) if end else 2017
 
@@ -23,44 +23,46 @@ class IMDBSpider(CrawlSpider):
     def start_requests(self):
         print "=====================start_requests================================"
         for year in range(self.start_year, self.end_year+1):
-            # sorted by number of votes
+            # movies are sorted by number of votes
             yield scrapy.Request('http://www.imdb.com/search/title?year={year},{year}&title_type=feature&sort=num_votes,desc'.format(year=year))
 
     def parse_page(self, response):
-        global TOTAL_PAGES, PASSED_PAGES
-        if PASSED_PAGES == TOTAL_PAGES:
-            raise CloseSpider("Reached page limit {}".format(TOTAL_PAGES))
-        print "=====================parse_page================================"
+        if self.passed_pages == self.total_pages:
+            raise CloseSpider("Reached page limit {}".format(self.total_pages))
 
+        print "=====================parse_page================================"
         content = response.xpath("//div[@class='lister-item-content']")
         paths = content.xpath("h3[@class='lister-item-header']/a/@href").extract() # list of paths of movies in the current page
 
-        # for path in paths:
-        #     item = MovieItem()
-        #     item['MainPageUrl'] = "http://imdb.com" + path
-        #     request = scrapy.Request(item['MainPageUrl'], callback=self.parse_movie_details)
-        #     request.meta['item'] = item
-        #     yield request
+        # all movies
+        self.passed_pages += 1
+        for path in paths:
+            item = MovieItem()
+            item['MainPageUrl'] = IMDB_URL + path
+            request = scrapy.Request(item['MainPageUrl'], callback=self.parse_movie_details)
+            request.meta['item'] = item
+            yield request
 
-        # single movie
-        path = paths[0]
-        item = MovieItem()
-        item['MainPageUrl'] = IMDB_URL + path
-        request = scrapy.Request(item['MainPageUrl'], callback=self.parse_movie_details)
-        request.meta['item'] = item
-        PASSED_PAGES += 1
-        yield request
+        # # single movie
+        # self.passed_pages += 1
+        # path = paths[0]
+        # item = MovieItem()
+        # item['MainPageUrl'] = IMDB_URL + path
+        # request = scrapy.Request(item['MainPageUrl'], callback=self.parse_movie_details)
+        # request.meta['item'] = item
+        # yield request
 
     parse_start_url = parse_page
 
     def parse_movie_details(self, response):
         print "=====================parse_movie_details================================"
         item = response.meta['item']
+        print item
         synopsis_url = response.xpath("//a[text()='Plot Synopsis']/@href").extract()
         url = item['MainPageUrl']
         title = response.xpath("//div[@class='titleBar']/div[@class='title_wrapper']/h1/text()").extract()[0][:-1]
-        if synopsis_url:
-            item['ID'] = url[url.find('/tt')+3:url.find('/?')] # to ignore 'tt', change to +3
+        if synopsis_url: # there is synopsis
+            item['ID'] = url[url.find('/tt')+3:url.find('/?')] # +3 to ignore '/tt',
             item['Title'] = title
             item['Year'] = response.xpath("//div[@class='titleBar']/div[@class='title_wrapper']/h1/span/a/text()").extract()[0]
             item['Director'] = response.xpath("//div/span[@itemprop='director']/a/span/text()").extract()
@@ -79,7 +81,7 @@ class IMDBSpider(CrawlSpider):
     def parse_synopsis(self,response):
         print "=====================parse_synopsis================================"
         item = response.meta['item']
-        synopsis = response.xpath("//div[@id='swiki.2.1']/text()").extract()[0] # list of paragraphs
+        synopsis = response.xpath("//div[@id='swiki.2.1']/text()").extract() # list of paragraphs
         item['Synopsis'] = synopsis
         print item
 
