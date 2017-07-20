50a14f2ab7856d5fb6bd3f2b62639fd7b1dd069b
dorseg <dor.segal@gmail.com>
2017-07-20 15:10:17 +0300

    added movie details class and writing to json

---
+++ MovieDetails.py
@@ -0,0 +1,9 @@
+class MovieDetails(object):
+    def __init__(self, title, year, genres, directors, rating, filming_locations, synopsis):
+        self._title = title
+        self._year = year
+        self._genres = genres
+        self._directors = directors
+        self._rating = rating
+        self._filming_locations = filming_locations
+        self._synopsis = synopsis
--- fetchData.py
+++ fetchData.py
@@ -1,4 +1,7 @@
 from imdb import IMDb
+import MovieDetails
+import json
+
 ia = IMDb()
 
 movie_ids = []
@@ -9,10 +12,13 @@ with open("crawler/ids/2016_2016.txt", 'r') as f:
 for id in movie_ids:
     movie = ia.get_movie(id)
     ia.update(movie, ['synopsis', 'locations'])  # fetch the 'synopsis' and 'locations' data sets.
-    synopsis = movie.get('synopsis')
     title = movie.get('title')
     year = movie.get('year')
     genres = movie.get('genres')  # list of unicode strings
-    rating = movie.get('rating')
-    locations = [location[:location.find('::')] for location in movie.get('locations')] # list of names
     directors = [director.get('name') for director in movie.get('director')]  # list of names
+    rating = movie.get('rating')
+    filming_locations = [location[:location.find('::')] for location in movie.get('locations')] # list of names
+    synopsis = movie.get('synopsis')
+    movie_details = MovieDetails(title, year, genres, directors, rating, filming_locations, synopsis)
+    with open("data/{}.txt".format(id), 'w') as out:
+        json.dump(movie_details, out)
