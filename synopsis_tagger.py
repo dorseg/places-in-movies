2e7ee7cedffcf9e95e09f9786982df9d8ad11f99
dorseg <dor.segal@gmail.com>
2017-07-21 10:45:31 +0300

    updated .gitignore to ignore the data folder

--- synopsis_tagger.py
+++ synopsis_tagger.py
@@ -5,7 +5,8 @@ import json
 # java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -port 9191
 # in stanford ner tagger folder
 
-with open("data/2975590.txt", 'r') as f:
+id = 0
+with open("data/{}.txt".format(id), 'r') as f:
     movie_details = json.loads(f.read())
     synopsis = movie_details['synopsis']
 
