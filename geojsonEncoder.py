9adcb956f5fd0c0e1f2b48659dd1e1c2f9abd1e2
dorseg <dor.segal@gmail.com>
2017-07-28 17:59:21 +0300

    added geo-json encoder

---
+++ geojsonEncoder.py
@@ -0,0 +1,49 @@
+import ner, json, geojson, os
+from opencage.geocoder import OpenCageGeocode
+from geojson import MultiPoint, Feature, FeatureCollection
+
+# first, run:
+# java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -port 9191
+# in stanford ner tagger folder
+
+tagger = ner.SocketNER(host='localhost', port=9191, output_format='slashTags')
+key = '42bae5691544417f8d1a7922f8bc9d48'
+geocoder = OpenCageGeocode(key)
+
+features = []
+
+def get_geolocation(loc):
+    cord = ()
+    result = geocoder.geocode(loc)
+    if result and len(result):
+        lng = result[0]['geometry']['lng']
+        lat = result[0]["geometry"]['lat']
+        cord = (lng,lat)
+    return cord
+
+
+print "Start parsing..."
+files = os.listdir(u"data_example")
+for file in files:
+    with open("data_example/{}".format(file), 'r') as f:
+        print "====Parsing file {}====".format(file)
+        movie_details = json.loads(f.read())
+        synopsis = movie_details['synopsis']
+        tagged_synopsis = tagger.get_entities(synopsis)
+        if 'LOCATION' not in tagged_synopsis:
+            print "No locations, continue..."
+            continue
+        locations = tagged_synopsis['LOCATION']
+        locations = list(set(tagged_synopsis['LOCATION']))
+        geo_locations = filter(lambda cord: len(cord) > 0, map(get_geolocation, locations))
+        points = MultiPoint(geo_locations)
+        feature = Feature(geometry=points, properties=movie_details)
+        features.append(feature)
+
+collection = FeatureCollection(features)
+with open("movies.geojson", 'w') as out:
+    geojson.dump(collection, out)
+
+
+
+
