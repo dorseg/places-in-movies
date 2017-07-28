1eaa6b799ad1ff8274c49bd41dedbba6965d2b51
dorseg <dor.segal@gmail.com>
2017-07-28 14:35:46 +0300

    map - first version

--- synopsis_tagger.py
+++ synopsis_tagger.py
@@ -1,16 +1,17 @@
-import ner
-import json
+import ner, json
 
 # first, run:
 # java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -port 9191
 # in stanford ner tagger folder
 
-id = 0
+tagger = ner.SocketNER(host='localhost', port=9191, output_format='slashTags')
+
+id = "1985_1995/0088889"
+
 with open("data/{}.txt".format(id), 'r') as f:
     movie_details = json.loads(f.read())
     synopsis = movie_details['synopsis']
 
-tagger = ner.SocketNER(host='localhost', port=9191, output_format='slashTags')
 tagged_synopsis = tagger.get_entities(synopsis)
 locations = list(set(tagged_synopsis['LOCATION']))
 print locations
