2eaa0faf082189db033fb6282e2fe81adb6c1d2a
dorseg <dor.segal@gmail.com>
2017-07-29 17:57:32 +0300

    better appereance

--- web/script.js
+++ web/script.js
@@ -19,10 +19,20 @@ var geojsonLayer = omnivore.geojson('movies.geojson', null, L.mapbox.featureLaye
   });
 
 
-var markerList = document.getElementById('marker-list');
+var markerList = document.getElementById('listings');
 
 function title_with_year(properties) {
   return properties.title + " (" + properties.year + ")";
+}
+
+function setActive(el) {
+  var siblings = markerList.getElementsByTagName('div');
+  for (var i = 0; i < siblings.length; i++) {
+    siblings[i].className = siblings[i].className
+    .replace(/active/, '').replace(/\s\s*$/, '');
+  }
+
+  el.className += ' active';
 }
 
 function onmove() {
@@ -36,12 +46,24 @@ function onmove() {
         // with the current map bounds.
         if (bounds.contains(marker.getLatLng()) && $.inArray(title, inBounds) == -1) {
             inBounds.push(title);
-            var item = markerList.appendChild(document.createElement('li'));
-            item.innerHTML = title;
-            item.onclick = function() {
+            var item = markerList.appendChild(document.createElement('div'));
+            item.className = 'item';
+            var link = item.appendChild(document.createElement('a'));
+            link.href = '#';
+            link.className = 'title';
+            link.innerHTML = title;
+            link.onclick = function() {
+              setActive(item);
               map.setView(marker.getLatLng(), 10);
               marker.openPopup();
             };
+            // Marker interaction
+            marker.on('click', function(e) {
+              // 1. center the map on the selected marker.
+              map.panTo(marker.getLatLng());
+              // 2. Set active the markers associated listing.
+              setActive(item);
+            });
         }
     });
 }
