566528155692541773c944087e0d663f9cd29648
dorseg <dor.segal@gmail.com>
2017-07-29 20:27:52 +0300

    bug in geocoder fixed

--- geojsonEncoder.py
+++ geojsonEncoder.py
@@ -6,6 +6,7 @@ from geojson import Point, Feature, FeatureCollection
 # java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -port 9191
 # in stanford ner tagger folder
 
+
 class GeoLocation(object):
     """
     Store GeoLocation details
@@ -26,7 +27,6 @@ tagger = ner.SocketNER(host='localhost', port=9191, output_format='slashTags')
 key = '42bae5691544417f8d1a7922f8bc9d48'
 geocoder = OpenCageGeocode(key)
 
-features = []
 
 def get_geolocation(loc):
     geo = None
@@ -38,39 +38,59 @@ def get_geolocation(loc):
     return geo
 
 
-print "Start parsing..."
-files = os.listdir(u"data_example")
-for file in files:
-    with open("data_example/{}".format(file), 'r') as f:
-        print "====Parsing file {}====".format(file)
-        movie_details = json.loads(f.read())
-        synopsis = movie_details['synopsis']
-        tagged_synopsis = tagger.get_entities(synopsis)
-        if 'LOCATION' not in tagged_synopsis:
-            print "No locations, continue..."
-            continue
-        locations = tagged_synopsis['LOCATION']
-        locations = list(set(tagged_synopsis['LOCATION']))  # remove duplicates
-        geolocations = filter(lambda x: x is not None, map(get_geolocation, locations))
-        if not geolocations:
-            print "No Geolocations, continue..."
-            continue
-        ignore_keys = ["filming_locations", "synopsis", "id"]
-        movie_details = {key: movie_details[key] for key in movie_details if key not in ignore_keys}
-        movie_details["marker-color"] = "0044FF"
-        movie_details["marker-symbol"] = "cinema"
-        for geo in geolocations:
-            print geo
-            props = movie_details.copy()
-            props["location"] = geo.location
-            point = Point(geo.get_cords())
-            feature = Feature(geometry=point, properties=props)
-            features.append(feature)
+def encode_to_geojson(data_folder):
+    print "Start encoding..."
+    features = []
+    files = os.listdir(data_folder)
+    for file in files:
+        with open("{}/{}".format(data_folder, file), 'r') as f:
+            print "====Parsing file {}====".format(file)
+            movie_details = json.loads(f.read())
+            synopsis = movie_details['synopsis']
+            tagged_synopsis = tagger.get_entities(synopsis)
+            if 'LOCATION' not in tagged_synopsis:
+                print "No locations, continue..."
+                continue
+            locations = list(set(tagged_synopsis['LOCATION']))  # remove duplicates
+            try:
+                geolocations = filter(lambda x: x is not None, map(get_geolocation, locations))
+            except Exception as e:
+                print e
+                return features
+            if not geolocations:
+                print "No Geolocations, continue..."
+                continue
+            ignore_keys = ["filming_locations", "synopsis", "id"]
+            movie_details = {key: movie_details[key] for key in movie_details if key not in ignore_keys}
+            movie_details["marker-color"] = "0044FF"
+            movie_details["marker-symbol"] = "cinema"
+            for geo in geolocations:
+                #print geo
+                props = movie_details.copy()
+                props["location"] = geo.location
+                point = Point(geo.get_cords())
+                feature = Feature(geometry=point, properties=props)
+                features.append(feature)
+    print "Created {} featurs".format(len(features))
+    return features
 
-print "Created {} featurs".format(len(features))
-collection = FeatureCollection(features)
-with open("movies.geojson", 'w') as out:
-    geojson.dump(collection, out)
+
+def write_to_file(features):
+    collection = FeatureCollection(features)
+    with open("movies.geojson", 'w') as out:
+        geojson.dump(collection, out)
+
+
+def main():
+    data_folder = u"data/1985_1995"
+    features = encode_to_geojson(data_folder)
+    write_to_file(features)
+
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
