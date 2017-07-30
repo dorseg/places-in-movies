f5de679f6a0d06329fbafa4495ec1dac442bda43
dorseg <dor.segal@gmail.com>
2017-07-30 22:05:45 +0300

    ready to encode big data

--- geojsonEncoder.py
+++ geojsonEncoder.py
@@ -26,6 +26,8 @@ class GeoLocation(object):
 tagger = ner.SocketNER(host='localhost', port=9191, output_format='slashTags')
 key = '42bae5691544417f8d1a7922f8bc9d48'
 geocoder = OpenCageGeocode(key)
+features = []
+limit = 100
 
 
 def get_geolocation(loc):
@@ -34,15 +36,18 @@ def get_geolocation(loc):
     if result and len(result):
         lng = result[0]['geometry']['lng']
         lat = result[0]["geometry"]['lat']
-        geo = GeoLocation(loc,lng,lat)
+        geo = GeoLocation(loc, lng, lat)
     return geo
 
 
 def encode_to_geojson(data_folder):
-    print "Start encoding..."
-    features = []
+    counter = 0
     files = os.listdir(data_folder)
+    print "Start encoding files in {}".format(data_folder)
     for file in files:
+        if counter == limit:
+            break
+        counter += 1
         with open("{}/{}".format(data_folder, file), 'r') as f:
             print "====Parsing file {}====".format(file)
             movie_details = json.loads(f.read())
@@ -56,7 +61,7 @@ def encode_to_geojson(data_folder):
                 geolocations = filter(lambda x: x is not None, map(get_geolocation, locations))
             except Exception as e:
                 print e
-                return features
+                break
             if not geolocations:
                 print "No Geolocations, continue..."
                 continue
@@ -71,19 +76,20 @@ def encode_to_geojson(data_folder):
                 point = Point(geo.get_cords())
                 feature = Feature(geometry=point, properties=props)
                 features.append(feature)
-    print "Created {} featurs".format(len(features))
-    return features
 
 
-def write_to_file(features):
+def write_to_file():
     collection = FeatureCollection(features)
     with open("movies.geojson", 'w') as out:
         geojson.dump(collection, out)
 
 
 def main():
-    data_folder = u"data/1985_1995"
-    features = encode_to_geojson(data_folder)
+    data_folder1 = u"data/1985_1995"
+    data_folder2 = u"data/2005_2015"
+    encode_to_geojson(data_folder1)
+    encode_to_geojson(data_folder2)
+    print "Created {} featurs".format(len(features))
     write_to_file(features)
 
 
